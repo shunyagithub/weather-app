39486b398c6c910400a0768884754765bdc16b52
shunyagithub <56504519+shunyagithub@users.noreply.github.com>
2020-09-15 02:12:40 +0900

    add icons

--- main.js
+++ main.js
@@ -9,6 +9,15 @@ const humidity = document.getElementById("humidity");
 const minMaxTemp = document.getElementById("minmax__temperature");
 const btnCelsius = document.getElementById("celsius");
 const btnFahrenheit = document.getElementById("fahrenheit");
+const clearSky = document.getElementById("clear-sky");
+const rain = document.getElementById("rain");
+const snow = document.getElementById("snow");
+const thunderstorm = document.getElementById("thunderstorm");
+const mist = document.getElementById("mist");
+const clouds = document.getElementById("broken-clouds");
+const body = document.body;
+
+const weathers = [clearSky, rain, clouds, snow, thunderstorm, mist];
 
 // Prepare openweathermap.org request
 let xml = new XMLHttpRequest();
@@ -20,6 +29,7 @@ const apikey = "&APPID=d4d9dff7aac7ef697172af5cc9bf274e";
 
 cityForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const chosenCity = cityInput.value;
 
   if (btnCelsius.classList.contains("active")) {
@@ -29,6 +39,7 @@ cityForm.addEventListener("submit", (e) => {
     xml.open("GET", url + chosenCity + unitsImperial + apikey);
     xml.send();
   }
+  cityForm.reset();
 });
 
 xml.onreadystatechange = () => {
@@ -39,7 +50,9 @@ xml.onreadystatechange = () => {
 
     const respons = JSON.parse(xml.response);
 
-    weatherCondition.textContent = respons.weather[0].main;
+    const weatherName = respons.weather[0].main;
+
+    weatherCondition.textContent = weatherName;
     locationName.textContent = respons.name;
     temperature.textContent = Math.floor(respons.main.temp) + "° ";
     minMaxTemp.textContent =
@@ -48,6 +61,26 @@ xml.onreadystatechange = () => {
       Math.floor(respons.main.temp_max) +
       "° ";
     humidity.textContent = respons.main.humidity + "%";
+
+    if (weatherName === "Clear") {
+      switchWeather(clearSky);
+      body.style.background = "#fffef4";
+    } else if (weatherName === "Rain") {
+      switchWeather(rain);
+      body.style.background = "#F4FFFE";
+    } else if (weatherName === "Clouds") {
+      switchWeather(clouds);
+      body.style.background = "#F2F2F2";
+    } else if (weatherName === "Snow") {
+      switchWeather(snow);
+      body.style.background = "#E6FFF6";
+    } else if (weatherName === "Thunderstorm") {
+      switchWeather(thunderstorm);
+      body.style.background = "#E5E5E5";
+    } else {
+      switchWeather(mist);
+      body.style.background = "#FFFFFF";
+    }
   }
 };
 
@@ -62,3 +95,31 @@ btnCelsius.addEventListener("click", () => {
   btnCelsius.classList.add("active");
   btnFahrenheit.classList.remove("active");
 });
+
+// Switch Weather Icons
+function removeShow() {
+  for (let eachWeather of weathers) {
+    eachWeather.classList.remove("show");
+  }
+}
+
+function addShow(target) {
+  target.classList.add("show");
+}
+
+function switchWeather(target) {
+  const tl = gsap
+    .timeline({})
+    .to(".show", {
+      opacity: 0,
+      duration: 0.5,
+      ease: "power1.out",
+    })
+    .add(removeShow())
+    .to(target, {
+      opacity: 1,
+      duration: 1,
+      ease: "power1.out",
+      onComplete: addShow(target),
+    });
+}
